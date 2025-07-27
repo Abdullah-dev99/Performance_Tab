1c7f6a0e85158ead14a7aa02b23261ff46adb9ec
abdullahvisuals99 <abdullahvisuals99@gmail.com>
2025-07-27 19:34:20 +0000

    Changes before Firebase Studio auto-run

--- obj/Debug/net9.0/myapp.AssemblyInfo.cs
+++ obj/Debug/net9.0/myapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c4424f0bab9fe014d3ede8db5ac80298d25e3e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21125cf6cc909e4f8aeba03a28a49e9d159ac295")]
 [assembly: System.Reflection.AssemblyProductAttribute("myapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
